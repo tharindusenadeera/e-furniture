31f6bcfee449198d70253346e7f084ff2b00cd01
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-10-22 11:56:28 +0100

    Identified the screens that should developed

---
+++ screens/CartScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const CartScreen = () => {
+  return (
+    <View>
+      <Text>Cart Screen</Text>
+    </View>
+  );
+};
+
+export default CartScreen;
