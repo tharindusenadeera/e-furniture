600ba1d09bb3804e8da5ca22d8b307c582b7fcc3
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-10-23 10:37:27 +0100

    Project reset

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native';
+import * as React from 'react';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
