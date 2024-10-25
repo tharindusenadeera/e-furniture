a9ba34c5891f0a57e1c22b3651535961a8073e7d
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-10-25 23:30:03 +0100

    Fonts loaded using expo-fonts hook

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,16 @@
 import { Text, View } from 'react-native';
 import * as React from 'react';
+import { useFonts } from 'expo-font';
 
 export default function Index() {
+  const [fontLoaded] = useFonts({
+    regular: require('../assets/fonts/Poppins-Regular.ttf'),
+    light: require('../assets/fonts/Poppins-Light.ttf'),
+    medium: require('../assets/fonts/Poppins-Medium.ttf'),
+    bold: require('../assets/fonts/Poppins-Bold.ttf'),
+    extrabold: require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    semiBold: require('../assets/fonts/Poppins-SemiBold.ttf'),
+  });
   return (
     <View
       style={{
