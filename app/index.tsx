8900b09d167258428f3ad6d12b7b0ed23b20790a
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-10-25 23:39:46 +0100

    Splashscreen hide done when fonts do load

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from 'react-native';
 import * as React from 'react';
 import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function Index() {
   const [fontLoaded] = useFonts({
@@ -11,6 +12,17 @@ export default function Index() {
     extrabold: require('../assets/fonts/Poppins-ExtraBold.ttf'),
     semiBold: require('../assets/fonts/Poppins-SemiBold.ttf'),
   });
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (fontLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+
+  if (!fontLoaded) {
+    return null;
+  }
+
   return (
     <View
       style={{
