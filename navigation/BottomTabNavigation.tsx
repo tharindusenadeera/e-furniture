77742c6e86504d9a083fc6f270a68aa3420fb8f6
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-11-01 15:39:51 +0000

    home icon added

--- navigation/BottomTabNavigation.tsx
+++ navigation/BottomTabNavigation.tsx
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '@/screens/HomeScreen';
 import SearchScreen from '@/screens/SearchScreen';
 import ProfileScreen from '@/screens/ProfileScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -24,7 +25,15 @@ const screenOptions: any = {
 const BottomTabNavigation = () => {
   return (
     <Tab.Navigator screenOptions={screenOptions}>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: (focused) => {
+            return <Ionicons name={focused ? 'home' : 'home-outline'} size={24} />;
+          },
+        }}
+      />
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
