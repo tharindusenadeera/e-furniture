f19d6bb911e597b2e80c805dbe09571d1b47b120
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-11-11 18:47:41 +0000

    Appbar styles added

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,17 @@
 import { View, Text, SafeAreaView } from 'react-native';
 import React from 'react';
 import styles from './styles/home.style';
+import { Ionicons } from '@expo/vector-icons';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <Text>HomeScreen</Text>
+      <View style={styles.appBarWrapper}>
+        <View style={styles.appBar}>
+          <Ionicons name="location-outline" size={24} />
+          <Text style={styles.location}> Sri Lanka</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
--- screens/styles/home.style.ts
+++ screens/styles/home.style.ts
@@ -1,6 +1,25 @@
 import { StyleSheet } from 'react-native';
 import { COLORS, SIZES } from '@/constants';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    fontFamily: 'bold',
+    fontSize: 40,
+  },
+  appBarWrapper: {
+    marginHorizontal: 22,
+    marginTop: SIZES.small,
+  },
+  appBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  location: {
+    fontFamily: 'semibold',
+    fontSize: SIZES.medium,
+    color: COLORS.gray,
+  },
+});
 
 export default styles;
